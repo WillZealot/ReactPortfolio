d14b77bd7732a51c321714f9d3ffcafeaf8a4ffd
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-07 11:39:27 -0400

    added an about me card inside the about me area need to refine it a bit more and start imlementing all the other components. its getting alot easier to use mui

--- portfolio/src/assets/components/aboutme.js
+++ portfolio/src/assets/components/aboutme.js
@@ -1,57 +1,85 @@
 import '../css/app.css'
-//import myPicture from '../images/Picsart_23-07-04_19-52-40-412.png'
+import myPicture from '../images/Picsart_23-07-04_19-52-40-412.png'
 
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import MuiGrid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
 
-const Grid = styled(MuiGrid)(({ theme }) => ({
-  width: '100%',
-  ...theme.typography.body2,
-  '& [role="separator"]': {
-    margin: theme.spacing(0, 2),
-  },
-}));
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
 
-let style = {
-    container: {
-        display:'flex',
-        flexWrap:'wrap',
-        textAlign:'left',
-        margin:'auto',
-        paddingTop:'120px',
-        maxWidth: '700px',
-    },
-    aboutmetext: {
-        color:'white',
-        fontSize:'20px'
-    },
-}
-
-export default function AboutMe() {
-  const rightContent = (
-    <div style={style.aboutmetext}>
-      {`I'm a versatile software developer with expertise in both frontend and backend development, specializing in the MERN (MongoDB, Express.js, React, Node.js) stack. My skill set includes JavaScript, Node.js, CSS, HTML, MySQL, and NoSQL databases.`}
-    </div>);
-    const leftContent = (
-        <div style={style.aboutmetext}>
-            {` I have a knack for crafting seamless user experiences, whether it's building web applications, Progressive Web Apps (PWAs), or RESTful APIs. With a passion for staying up-to-date with the latest technologies, I'm eager to tackle diverse challenges and contribute to innovative projects.`}
-        </div>
-    )
-  
-
+export default function UserCard() {
   return (
-    <Grid container style={style.container}>
-      <Grid item xs>
-        {rightContent}
-      </Grid>
-      <Divider orientation="vertical" flexItem>
-        <p style={style.aboutmetext}>&</p>
-      </Divider>
-      <Grid item xs>
-        {leftContent}
-      </Grid>
-    </Grid>
+    <Box
+      sx={{
+        paddingTop:'50px',
+        margin:'auto',
+        width: "100%",
+        maxWidth: "700px",
+        position: "relative",
+        overflow: { xs: "auto", sm: "initial" }
+      }}
+    >
+      <Box
+      />
+      <Card
+        orientation="horizontal"
+        sx={{
+          width: "100%",
+          flexWrap: "wrap",
+          [`& > *`]: {
+            "--stack-point": "500px",
+            minWidth:
+              "clamp(0px, (calc(var(--stack-point) - 2 * var(--Card-padding) - 2 * var(--variant-borderWidth, 0px)) + 1px - 100%) * 999, 100%)"
+          },
+          // make the card resizable for demo
+          overflow: "auto",
+          resize: "horizontal"
+        }}
+      >
+        <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
+          <img
+            src={myPicture}
+            srcSet={myPicture}
+            loading="lazy"
+            alt=""
+          />
+        </AspectRatio>
+        <CardContent>
+          <Typography fontSize="xl" fontWeight="lg">
+            William Tikhonenko
+          </Typography>
+          <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
+            Full Stack Developer
+          </Typography>
+          <Sheet
+            sx={{
+              bgcolor: "background.level1",
+              borderRadius: "sm",
+              p: 1.5,
+              my: 1.5,
+              display: "flex",
+              gap: 2,
+              "& > div": { flex: 1 }
+            }}
+          >
+            <div>
+              <Typography level="body-lg" fontWeight="lg">
+                About Me
+              </Typography>
+              <Typography fontWeight="sm">
+                I'm a versatile software developer located in Florida. With
+                expertise in both frontend and backend development, specializing
+                in the MERN (MongoDB, Express.js, React, Node.js) stack. My
+                skill set includes JavaScript, Node.js, CSS, HTML, MySQL, and
+                NoSQL databases.
+              </Typography>
+            </div>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
