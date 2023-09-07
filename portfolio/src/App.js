1c5884ff9ae4cabd669713b819ed7fe4cb3cb238
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-07 15:51:43 -0400

    trying to make footer but its sticking the the bottom of viewport lol

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,20 +3,20 @@ import './assets/css/app.css'
 import Header from './assets/components/header';
 import Nav from './assets/components/nav';
 import AboutMe from './assets/components/aboutme';
-import Resume from './assets/components/resume';
-import Projects from './assets/components/project';
+//import Resume from './assets/components/resume';
+//import Projects from './assets/components/project';
 import Footer from './assets/components/footer'; 
 
 function App() {
   return (
-  <div>
+    <div>
     <Header></Header>
     <Nav></Nav>
     <AboutMe></AboutMe>
-    <Projects></Projects>
-    <Resume></Resume>
     <Footer></Footer>
-  </div>
+    </div>
+//      <Projects></Projects>
+//      <Resume></Resume>
     
    
     
--- portfolio/src/assets/components/footer.js
+++ portfolio/src/assets/components/footer.js
@@ -1,29 +1,29 @@
-import '../css/app.css'
 import React from 'react';
 
-export default function Footer(){
+export default function Footer() {
   return (
-    <footer style={footerStyle}>
-      <div className="icon">Icon 1</div>
-      <div className="icon">Icon 2</div>
-      <div className="icon">Icon 3</div>
-    </footer>
+    <div style={containerStyle}>
+      <footer style={footerStyle}>
+        <div className="icon">Icon 1</div>
+        <div className="icon">Icon 2</div>
+        <div className="icon">Icon 3</div>
+      </footer>
+    </div>
   );
-};
+}
 
-const footerStyle = {
-    position: 'sticky',    // Make the footer sticky
-    bottom: '0',           // Stick it to the bottom of the viewport
+const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh', // Ensure the container takes the full viewport height
+  };
+  
+  const footerStyle = {
+    marginTop: 'auto', // Push the footer to the bottom
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    color: '#333',
+    height: '60px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.7)', // White background with 0.7 opacity
-    color: '#333',         // Text color
-    height: '60px',
-    // Add more styles as needed
   };
-
-
-
-
-
