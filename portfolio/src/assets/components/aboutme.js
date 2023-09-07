1134f370b203f11af7b0a5174bdcc73843f6da0f
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-09-06 21:49:02 -0400

    about me is mroe edited now to edtfooter then get into the technical spooky stuff like form and contactr and project

--- portfolio/src/assets/components/aboutme.js
+++ portfolio/src/assets/components/aboutme.js
@@ -14,25 +14,40 @@ const Grid = styled(MuiGrid)(({ theme }) => ({
   },
 }));
 
+let style = {
+    container: {
+        display:'flex',
+        flexWrap:'wrap',
+        textAlign:'left',
+        margin:'auto',
+        paddingTop:'120px',
+        maxWidth: '700px',
+    },
+    aboutmetext: {
+        color:'white',
+        fontSize:'20px'
+    },
+}
+
 export default function AboutMe() {
   const rightContent = (
-    <div>
+    <div style={style.aboutmetext}>
       {`I'm a versatile software developer with expertise in both frontend and backend development, specializing in the MERN (MongoDB, Express.js, React, Node.js) stack. My skill set includes JavaScript, Node.js, CSS, HTML, MySQL, and NoSQL databases.`}
     </div>);
     const leftContent = (
-        <div>
+        <div style={style.aboutmetext}>
             {` I have a knack for crafting seamless user experiences, whether it's building web applications, Progressive Web Apps (PWAs), or RESTful APIs. With a passion for staying up-to-date with the latest technologies, I'm eager to tackle diverse challenges and contribute to innovative projects.`}
         </div>
     )
   
 
   return (
-    <Grid container>
+    <Grid container style={style.container}>
       <Grid item xs>
         {rightContent}
       </Grid>
       <Divider orientation="vertical" flexItem>
-        &
+        <p style={style.aboutmetext}>&</p>
       </Divider>
       <Grid item xs>
         {leftContent}
--- portfolio/src/assets/components/footer.js
+++ portfolio/src/assets/components/footer.js
@@ -3,6 +3,13 @@ import '../css/app.css'
 export default function Footer() {
     return (
     <div className="Footer">
+        <footer>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </footer>
     </div>
     )
 }
