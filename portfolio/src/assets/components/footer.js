a3427841db6987f1515f7b1fac81ac41d1f57a3d
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-07 13:43:13 -0400

    workig on a proper footer

--- portfolio/src/assets/components/footer.js
+++ portfolio/src/assets/components/footer.js
@@ -1,15 +1,29 @@
 import '../css/app.css'
+import React from 'react';
 
-export default function Footer() {
-    return (
-    <div className="Footer">
-        <footer>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </footer>
-    </div>
-    )
-}
+export default function Footer(){
+  return (
+    <footer style={footerStyle}>
+      <div className="icon">Icon 1</div>
+      <div className="icon">Icon 2</div>
+      <div className="icon">Icon 3</div>
+    </footer>
+  );
+};
+
+const footerStyle = {
+    position: 'sticky',    // Make the footer sticky
+    bottom: '0',           // Stick it to the bottom of the viewport
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)', // White background with 0.7 opacity
+    color: '#333',         // Text color
+    height: '60px',
+    // Add more styles as needed
+  };
+
+
+
+
+
