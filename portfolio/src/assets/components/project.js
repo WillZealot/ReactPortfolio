fb02887d68aed03b34a58f51ce0823a2da0b8a30
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-09-08 22:49:54 -0400

    restarting app creation

--- portfolio/src/assets/components/project.js
+++
@@ -1,8 +0,0 @@
-import '../css/app.css'
-
-export default function Projects() {
-    return (
-    <div className="Projects">
-    </div>
-    )
-}
--- portfolio/src/assets/components/resume.js
+++
@@ -1,9 +0,0 @@
-import '../css/app.css'
-
-export default function Resume() {
-    return (
-        <div className="Resume">
-          <h5>Pretend This Is A Swaggy Resume</h5>
-    </div>
-    )
-}
