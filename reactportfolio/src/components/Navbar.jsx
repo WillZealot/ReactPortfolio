729f956357f6265678015d6b9f8faf8ab148884e
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-09-09 22:55:20 -0400

    nav bar component is styled up looks pretty

--- reactportfolio/src/components/Navbar.jsx
+++ reactportfolio/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from "react-icons/fa"
 import Logo from "../assets/logo2.png"
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#27374D] text-slate-100'>
         <div>
@@ -10,8 +14,7 @@ const Navbar = () => {
         </div>
 
         {/* menu */}
-        <div >
-            <ul className='flex'>
+            <ul className='md:flex hidden'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
@@ -19,20 +22,20 @@ const Navbar = () => {
                 <li>Work</li>
                 <li>Contact</li>
             </ul>
-        </div>
+        
 
         {/* Hamburger */}
-        <div className="hidden">
-            <FaBars />
+        <div onClick={handleClick} className="md:hidden z-10">
+            {!nav ? <FaBars/> : <FaTimes/>}
         </div>
         {/* Mobile Menu */}
-        <ul className="hidden">
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Experience</li>
-            <li>Work</li>
-            <li>Contact</li>
+        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#27374D] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Experience</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
         </ul>
 
 
