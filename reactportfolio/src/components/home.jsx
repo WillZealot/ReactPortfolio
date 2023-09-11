54ecd01e823873f5045460fff7f8786b0ca67728
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-09-11 00:21:47 -0400

    finished about section

--- reactportfolio/src/components/home.jsx
+++ reactportfolio/src/components/home.jsx
@@ -3,13 +3,13 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#27374D]'>
+    <div name='home' className='w-full h-screen bg-[#27374D]'>
     {/*Container  */}
     <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
     <p className='text-[#ccd6f6]'>Hi, my name is</p>
     <h1 className='text-4xl sm:text-7xl font-bold text-white'>William Tikhonenko</h1>
     <h2 className='text-4xl sm:text-7lx font-bold text-[#8892b0]'>I'm a full stack developer</h2>
-    <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer with a speciality in creating a digital experience you wont forget. Currently, I'm focused on building responsive unique full-stack applications and growing my understanding on more complex topics.</p>
+    <p className='text-[#BBBCBF] font-bold py-4 max-w-[700px]'>I'm a full-stack developer with a speciality in creating a digital experience you wont forget. Currently, I'm focused on building responsive unique full-stack applications and growing my understanding on more complex topics.</p>
     {/* Button */}
     <div>
       <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ccd6f6] hover:border-[#ccd6f6] hover:text-black'>View Work
