8aec8256ca3a270dc60742d6ff061c7bfa68805b
William Tikhonenko <williamtikhonenko@gmail.com>
2023-09-13 20:20:00 -0400

    changed title

--- reactportfolio/src/components/Navbar.jsx
+++ reactportfolio/src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const Navbar = () => {
         <div className="hidden lg:flex fixed flex-colum top-[30%] left-0">
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/feed/">LinkedIn <FaLinkedin size={30}></FaLinkedin></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/william-tikhonenko-36479b238/ß">LinkedIn <FaLinkedin size={30}></FaLinkedin></a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
                     <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/WillZealot">Github <FaGithub size={30}></FaGithub></a>
